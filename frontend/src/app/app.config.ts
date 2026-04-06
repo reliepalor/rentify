fb42133b0da5572b4ff78592d8bcd7b63f913a28
relipalor <reliepalor15@gmail.com>
2026-04-06 21:35:12 +0800

    use HashLocationStrategy to fix Vercel 404 routing

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    
+    // This line fixes the 404 issue on Vercel
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
