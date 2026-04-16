d6bc554b2fa060c2fff743ea09818c7590d329a8
relipalor <reliepalor15@gmail.com>
2026-04-16 23:00:39 +0800

    Prepare Angular for Vercel deployment

--- frontend/src/environment.prod.ts
+++ frontend/src/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   supabase: {
-    url: 'https://qcgelrtktmmkvxrpyevy.supabase.co',        
+    url: 'https://qcgelrtktmmkvxrpyevy.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjZ2VscnRrdG1ta3Z4cnB5ZXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4Njk3ODMsImV4cCI6MjA5MDQ0NTc4M30.Dsd-k2K1bX8xnCy0PkEsxWuVvDpKXExo8wiIQU4_4GM'
   }
 };
